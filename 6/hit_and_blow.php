74e1eacb5dcd012c620fc899a62a9abf925fa50c
EC2 Default User <ec2-user@ip-172-31-43-132.ap-northeast-1.compute.internal>
2020-05-18 00:50:09 +0000

    最新5/18 (2)

--- 6/hit_and_blow.php
+++ 6/hit_and_blow.php
@@ -1,14 +1,23 @@
 <?PHP
-$rand[0] = mt_rand(0,9);
-$rand[1] = mt_rand(0,9);
-$rand[2] = mt_rand(0,9);
-$rand[0] = 0;
-echo $rand[0].$rand[1].$rand[2]."\n";
-if ($rand[0] === $rand[1] && $rand[1] === $rand[2] && $rand[2] === $rand[0] && $rand[0] === "0" ) {
+do{
+$num = array (1,2,3,4,5,6,7,8,9,0);
+//$rand = array_rand ($num , 3 );
+shuffle($num);
+$rand = array_slice($num, 0, 3);
+print_r($rand);
+}while( $rand[0] == 0 );
+do{
+$input = fgets(STDIN);
+$input_array = str_split($input);
+print_r($input_array);
+echo count($input_array);
+}while( count ( $input_array ) != 3 );
+
+/*if ($rand[0] === $rand[1] && $rand[1] === $rand[2] && $rand[2] === $rand[0] && $rand[0] === "0" ) {
     $rand[0] = mt_rand(0,9);
     $rand[1] = mt_rand(0,9);
     $rand[2] = mt_rand(0,9);
-    echo $rand."\n";
-}
+    echo $rand[0].$rand[1].$rand[2]."\n";
+}*/
 echo "抜けたよ\n";
 ?>
--- hello/routes/web.php
+++ hello/routes/web.php
@@ -1,16 +1,27 @@
 <?php
+//①リスト一覧画面
+Route::get('/','ListingsController@index');
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
+//②リスト新規画面
+Route::get('/new', 'ListingsController@new')->name('new');
+
+//③リスト新規作成処理 
+Route::post('/listings','ListingsController@store');
+//===ここまで追加===
+/* ======= 解説 =======
+■ルートの定義とは、このURLでアクセスされた時は、このコントローラのアクションを呼び出してください」と定義します。
+    ①の場合、「/」でアクセスされたら、ListingsControllerのindexアクションを呼び出してねという支持になります
+
+Route::get('リクエストされたURL','呼び出すコントローラー名@処理するコントローラ名');
+
+②リスト新規画面の「->name('new')」の記述は、ルートの名前を定義しています。これを名前付きルートといいます。
+名前を定義しておくことで、コントローラやビューで定義した名前でパスの指定をすることができます。
+
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+?>
